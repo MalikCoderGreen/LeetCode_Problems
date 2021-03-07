573c8684ea56db28ff8290f4380f3ada94bd7c38
Maliak (Ma-Leek) Green <green.malik5@gmail.com>
2021-03-07 03:11:36 -0500

    Update start_end.py

--- medium_problems/start_end.py
+++ medium_problems/start_end.py
@@ -1,5 +1,5 @@
 """
-Find First and Last Position of Element in Sorted Array
+34.Find First and Last Position of Element in Sorted Array
 
 Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
 If target is not found in the array, return [-1, -1].
